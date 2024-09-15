18b9e41689b8c6e984355181da46c7334e6d1ee3
Ajay Gupta <ajaygupta@Ajays-MacBook-Pro.local>
2024-09-15 18:58:16 +0530

    mac setup

--- template.go
+++ template.go
@@ -4,11 +4,12 @@ import (
 	"bufio"
 	. "fmt"
 	"os"
-	"sort"
 )
 
 func solve(in *bufio.Reader, out *bufio.Writer) {
-
+	var x int
+	Fscan(in, &x)
+	Fprint(out, "yo", " ", x)
 }
 
 func main() {
@@ -18,47 +19,6 @@ func main() {
 	var T int
 	for Fscan(in, &T); T > 0; T-- {
 		solve(in, out)
-		Fprint(out, "\n")
 	}
 	Fprintln(out)
-}
-
-func min(nums ...int) int {
-	res := nums[0]
-	for _, v := range nums {
-		if v < res {
-			res = v
-		}
-	}
-	return res
-}
-
-func max(nums ...int) int {
-	res := nums[0]
-	for _, v := range nums {
-		if v > res {
-			res = v
-		}
-	}
-	return res
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func gcd(a, b int) int {
-	for b != 0 {
-		a, b = b, a%b
-	}
-	return a
-}
-
-func lowerBound(nums []int, target int) int {
-	return sort.Search(len(nums), func(i int) bool {
-		return nums[i] >= target
-	})
 }
